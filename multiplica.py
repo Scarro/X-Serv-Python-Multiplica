c36426501887050f75e9c2882a25d722d1ade4e0
scarro <sergioca22@gmail.com>
2016-02-15 18:32:32 +0100

    adaptado al enunciado del 13.3

--- multiplica.py
+++ multiplica.py
@@ -1,12 +1,9 @@
 #! /usr/bin/python
 #Programa en python que muestra por pantalla la tabla de multiplicar del numero introducido
-
-tabla = int(raw_input("Introduzca el numero de la tabla a mostrar:"));
 rango = range(1,11);
-cifras = str(tabla).__len__()	# averigua las cifras que tiene el numero introducido
-if tabla <0:
-	print("Uso: numero no valido, introduce otro numero (>0)");
-else:
-	print("Tabla del " + str(tabla) + "\n----------" + "-"*cifras);
-	for numero in range(1,11):
-		print(str(tabla) + " * " + str(numero) + " = " + str(tabla*numero));
+
+for numerando1 in rango:
+	cifras = str(numerando1).__len__();	# averigua las cifras que tiene el numero
+	print("Tabla del " + str(numerando1) + "\n----------" + "-"*cifras);
+	for numerando2 in rango:
+		print(str(numerando1) + " por " + str(numerando2) + " es " + str(numerando1*numerando2));
