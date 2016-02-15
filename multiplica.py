3bffba6d00511ffe7861de7474e67dd025d1d702
scarro <sergioca22@gmail.com>
2016-02-15 18:35:29 +0100

    cambios

--- multiplica.py
+++ multiplica.py
@@ -1,9 +1,9 @@
 #! /usr/bin/python
-#Programa en python que muestra por pantalla la tabla de multiplicar del numero introducido
+#Programa en python que muestra por pantalla las 10 primeras tablas de multiplicar
 rango = range(1,11);
 
 for numerando1 in rango:
-	cifras = str(numerando1).__len__();	# averigua las cifras que tiene el numero
+	cifras = str(numerando1).__len__();
 	print("Tabla del " + str(numerando1) + "\n----------" + "-"*cifras);
 	for numerando2 in rango:
 		print(str(numerando1) + " por " + str(numerando2) + " es " + str(numerando1*numerando2));
