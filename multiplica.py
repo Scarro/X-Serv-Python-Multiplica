582ae9fbfa39559117118f7c44b146c23819c5d5
Scarro <sergioca22@gmail.com>
2017-02-03 19:23:53 +0100

    new

--- multiplica.py
+++ multiplica.py
@@ -1,4 +1,6 @@
-#! /usr/bin/python
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
 #Sergio Carro Albarran
 #Programa en python que muestra por pantalla las 10 primeras tablas de multiplicar
 rango = range(1,11);
@@ -7,4 +9,4 @@ for numerando1 in rango:
 	cifras = str(numerando1).__len__();
 	print("Tabla del " + str(numerando1) + "\n----------" + "-"*cifras);
 	for numerando2 in rango:
-		print(str(numerando1) + " por " + str(numerando2) + " es " + str(numerando1*numerando2));
+		print(str(numerando1) + " por " + str(numerando2) + " = " + str(numerando1*numerando2));
